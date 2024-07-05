9decd68be0c095d180524d2ea49695b820a96169
username <bhanuprakashmunagala@gmail.com>
2024-07-05 18:13:59 +0530

    Added frontend script

---
+++ 01-frontend.sh
@@ -0,0 +1,12 @@
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
+
+systemctl enable nginx
+systemctl start nginx
+
+rm -rf /usr/share/nginx/html/*
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
+systemctl restart nginx
