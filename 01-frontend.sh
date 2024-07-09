8f6c43294e266a051c54ef9fcd4a231643e91a4f
bhanu.munagala <bhanuprakashmunagala@gmail.com>
2024-07-09 13:13:19 +0530

    added nginx and config file

--- 01-frontend.sh
+++ 01-frontend.sh
@@ -1,26 +1,26 @@
-echo "Install Nginx"
+echo "Install Nginx##################################################################################"
 
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
 
-echo "creating nginx configuration file"
+echo "copying nginx configuration file##################################################################################"
 cp nginx.conf /etc/nginx/nginx.conf
 
-echo "Removing the default content in html folder"
+echo "Removing the default content in html folder##################################################################################"
 
 rm -rf /usr/share/nginx/html/*
 
-echo "Downloading project content"
+echo "Downloading project content##################################################################################"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 
-echo "changing to the html directory"
+echo "changing to the html directory##################################################################################"
 cd /usr/share/nginx/html
 
-echo "unzipping the downloaded content here"
+echo "unzipping the downloaded content here##################################################################################"
 unzip /tmp/frontend.zip
 
-echo "enabling the service"
+echo "enabling the service##################################################################################"
 systemctl enable nginx
-echo "starting/restarting the webserver"
+echo "starting/restarting the webserver##################################################################################"
 systemctl restart nginx
