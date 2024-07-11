5a893fd867d1f8ee9a9a09a7fb7c5460eae9d30e
bhanu.munagala <bhanuprakashmunagala@gmail.com>
2024-07-11 12:56:56 +0530

    Added payment script and service file also empty dispat h script file

---
+++ 10-payment.sh
@@ -0,0 +1,29 @@
+echo "copying service file"
+cp payment.service /etc/systemd/system/payment.service
+
+echo "Install Python 3"
+dnf install python3 gcc python3-devel -y
+
+echo "Add application User"
+useradd roboshop
+
+echo "remove previous data so that it would not ask for confirmation to remove the data"
+rm -rf /app
+
+echo "setup an app directory."
+mkdir /app
+
+echo "Download the application code to created app directory."
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
+cd /app
+unzip /tmp/payment.zip
+
+echo "download the dependencies."
+cd /app
+pip3 install -r requirements.txt
+
+echo "Load the service and Start the service."
+systemctl daemon-reload
+systemctl enable payment
+systemctl start payment
+
